cf66af3f070351ec3f03cd5d9ac72be2134a62a3
FantasticFiasco <mattias.kindborg@gmail.com>
2017-07-02 11:07:39 +0200

    chore: trigger build

--- test/Placeholder.spec.ts
+++ test/Placeholder.spec.ts
@@ -1,3 +1,7 @@
+import { Placeholder } from './../src/Placeholder';
+
 describe('placeholder', () => {
-    it('is a placeholder');
+    it('is a placeholder', function() {
+        new Placeholder().add(1, 3);
+    });
 });
