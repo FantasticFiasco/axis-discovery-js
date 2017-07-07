03867f81ce0fda1b66c39491b4c1d209a4427e2b
FantasticFiasco <mattias.kindborg@gmail.com>
2017-07-07 21:53:14 +0200

    wip

--- test/caches/DeviceCache.spec.ts
+++ test/caches/DeviceCache.spec.ts
@@ -13,37 +13,48 @@ describe('DeviceCache', () => {
 
     describe('#update', () => {
 
-        it('should return device when cache holds more information', function() {
+        it('should return device when cache miss', function() {
+            // Arrange
+            const subject = new DeviceCache();
+
+            // Act
+            const actual = subject.update(DEVICE_WITHOUT_INFORMATION);
+
+            // Assert
+            actual.should.be.deep.equal(DEVICE_WITHOUT_INFORMATION);
+        });
+
+        it('should return device when cache hit holds more information', function() {
             // Arrange
             const subject = new DeviceCache(DEVICE_WITH_INFORMATION);
 
-            // Acr
+            // Act
             const actual = subject.update(DEVICE_WITHOUT_INFORMATION);
 
             // Assert
-            actual.should.be.be.deep.equal(DEVICE_WITH_INFORMATION);
+            actual.should.be.deep.equal(DEVICE_WITH_INFORMATION);
         });
 
-        it('should return device when cache holds less information', function() {
+        it('should return device when cache hit holds less information', function() {
             // Arrange
             const subject = new DeviceCache(DEVICE_WITHOUT_INFORMATION);
 
-            // Acr
+            // Act
             const actual = subject.update(DEVICE_WITH_INFORMATION);
 
             // Assert
-            actual.should.be.be.deep.equal(DEVICE_WITH_INFORMATION);
+            actual.should.be.deep.equal(DEVICE_WITH_INFORMATION);
         });
 
-        it('should return device when cache holds identical information', function() {
+        it('should return device when cache hit holds identical information', function() {
             // Arrange
             const subject = new DeviceCache(DEVICE_WITHOUT_INFORMATION);
 
-            // Acr
+            // Act
             const actual = subject.update(DEVICE_WITHOUT_INFORMATION);
 
             // Assert
-            actual.should.be.be.deep.equal(DEVICE_WITHOUT_INFORMATION);
+            actual.should.be.deep.equal(DEVICE_WITHOUT_INFORMATION);
         });
 
         it('should fail when device has no MAC address', function() {
