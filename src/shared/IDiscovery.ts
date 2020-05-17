27627e0c28b03f8906e657af03831dc7ed96e536
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2020-05-17 18:16:53 +0200

    chore: use typescript project references (#424)

--- src/shared/IDiscovery.ts
+++ src/shared/IDiscovery.ts
@@ -4,6 +4,6 @@ export interface IDiscovery {
     start(): Promise<void>;
     stop(): Promise<void>;
     search(): Promise<void>;
-    onHello(callback: (device: Device) => void);
-    onGoodbye(callback: (device: Device) => void);
+    onHello(callback: (device: Device) => void): void;
+    onGoodbye(callback: (device: Device) => void): void;
 }
